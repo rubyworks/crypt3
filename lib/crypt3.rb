2238b9e8368b59079e02c044849a43df851fd5d5
7rans <transfire@gmail.com>
2010-09-03 01:17:06 -0400

    It would appear that String#^ (xor) is not needed at all

--- lib/crypt3.rb
+++ lib/crypt3.rb
@@ -157,32 +157,27 @@ module Crypt3
     (1..size).collect { ITOA64[rand(ITOA64.size)].chr }.join("")
   end
 
-end
-
-
-class String
-
-  # Binary XOR of two strings. 
-  #
-  #   a = "\000\000\001\001" ^ "\000\001\000\001"
-  #   b = "\003\003\003" ^ "\000\001\002"
-  #
-  #   a  #=> "\000\001\001\000"
-  #   b  #=> "\003\002\001"
-  #
-  def ^(aString)
-    a = self.unpack('C'*(self.length))
-    b = aString.unpack('C'*(aString.length))
-    if (b.length < a.length)
-      (a.length - b.length).times { b << 0 }
-    end
-    xor = ""
-    0.upto(a.length-1) { |pos|
-      x = a[pos] ^ b[pos]
-      xor << x.chr()
-    }
-    return(xor)
-  end
+  #  # Binary XOR of two strings. 
+  #  #
+  #  #   a = xor("\000\000\001\001", "\000\001\000\001")
+  #  #   b = xor("\003\003\003", "\000\001\002")
+  #  #
+  #  #   a  #=> "\000\001\001\000"
+  #  #   b  #=> "\003\002\001"
+  #  #
+  #  def xor(string1, string2)
+  #    a = string1.unpack('C'*(self.length))
+  #    b = string2.unpack('C'*(aString.length))
+  #    if (b.length < a.length)
+  #      (a.length - b.length).times { b << 0 }
+  #    end
+  #    xor = ""
+  #    0.upto(a.length-1) { |pos|
+  #      x = a[pos] ^ b[pos]
+  #      xor << x.chr()
+  #    }
+  #    return(xor)
+  #  end
 
 end
 
